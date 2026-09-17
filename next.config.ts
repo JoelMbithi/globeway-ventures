73885247466e6368d86e28de4b1e4e8fe18c1fc5
Joel Mbithi <joellembithi@gmail.com>
2026-09-17 19:48:40 +0300

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/globeway-ventures',
+  assetPrefix: '/globeway-ventures/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
